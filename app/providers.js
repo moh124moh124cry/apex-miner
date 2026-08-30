42214b222c65443f46c778d14b934c7ebedbea89
moh124moh124cry <cherfaouimohamed377@gmail.com>
2026-08-30 01:00:17 +0100

    Update TonConnectUIProvider manifest URL

--- app/providers.js
+++ app/providers.js
@@ -3,7 +3,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 export function Providers({ children }) {
   return (
-    <TonConnectUIProvider manifestUrl="https://apex-miner-rho.vercel.app/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://apex-miner.vercel.app/tonconnect-manifest.json">
       {children}
     </TonConnectUIProvider>
   );
