96ff576caed9cb43d501d9ff0da1473f57acde02
moh124moh124cry <cherfaouimohamed377@gmail.com>
2026-09-08 17:11:50 +0100

    Refactor TonConnectUIProvider and script tag

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,29 @@
 "use client";
-import './globals.css';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+import "./globals.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
+const appUrl =
+  process.env.NEXT_PUBLIC_APP_URL ||
+  "https://apex-miner-rho.vercel.app";
+
+const tonConnectManifestUrl =
+  `${appUrl.replace(/\/$/, "")}/tonconnect-manifest.json`;
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://telegram.org/js/telegram-web-app.js" async></script>
+        <script
+          src="https://telegram.org/js/telegram-web-app.js"
+          async
+        ></script>
       </head>
+
       <body>
-        <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json">
+        <TonConnectUIProvider
+          manifestUrl={tonConnectManifestUrl}
+        >
           {children}
         </TonConnectUIProvider>
       </body>
