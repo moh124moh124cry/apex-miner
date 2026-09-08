407cefaa55f9a4fe84e8e4fdcdf676bfaca05865
moh124moh124cry <cherfaouimohamed377@gmail.com>
2026-09-08 17:24:37 +0100

    Add TonWalletButton component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
 "use client";
 
 import "./globals.css";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import {
+  TonConnectButton,
+  TonConnectUIProvider,
+} from "@tonconnect/ui-react";
+import { usePathname } from "next/navigation";
 
 const appUrl =
   process.env.NEXT_PUBLIC_APP_URL ||
@@ -10,7 +14,23 @@ const appUrl =
 const tonConnectManifestUrl =
   `${appUrl.replace(/\/$/, "")}/tonconnect-manifest.json`;
 
+function TonWalletButton() {
+  return (
+    <div className="flex flex-col items-end gap-1">
+      <span className="text-[9px] font-black text-[#0098EA] uppercase tracking-wider pr-1">
+        TON Wallet
+      </span>
+
+      <div className="origin-top-right scale-[0.85]">
+        <TonConnectButton />
+      </div>
+    </div>
+  );
+}
+
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <head>
@@ -20,10 +40,16 @@ export default function RootLayout({ children }) {
         ></script>
       </head>
 
-      <body>
+      <body className="relative">
         <TonConnectUIProvider
           manifestUrl={tonConnectManifestUrl}
         >
+          {pathname === "/" && (
+            <div className="absolute top-[70px] right-4 z-[70]">
+              <TonWalletButton />
+            </div>
+          )}
+
           {children}
         </TonConnectUIProvider>
       </body>
